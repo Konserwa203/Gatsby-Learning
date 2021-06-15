4a2528ca7f5585a5dd99ba64d1a420d21bbec307
Konserwa203 <MaciejKolber98@gmail.com>
2021-06-16 00:55:24 +0200

    Finished part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "LearningGatsbyEuvic",
+    title: "Gatsby 4 Euvic",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   
   
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p> I'm making this by following the Gatsby tutorial,and this is a children component</p>
-      <StaticImage
-        alt="A beautyful pet!"
-        src="https://images.unsplash.com/photo-1572650117973-7d78c3e9aedf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-      />
-        <br/> <br/>
+     
       <StaticImage 
         alt="A peaceful snake"
         src="../images/ballPython.jpg"
