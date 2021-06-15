d57c634ae1cf24a5dfa15d323dbbb0d7d821dc08
Konserwa203 <MaciejKolber98@gmail.com>
2021-06-16 00:06:52 +0200

    Finished part 3 of tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: "LearningGatsbyEuvic",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  
+  
+  ],
 };
