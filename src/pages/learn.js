6b2fd7746aa41f8532ba709fd9e13443d0cd5d83
Konserwa203 <MaciejKolber98@gmail.com>
2021-06-16 16:59:52 +0200

    styling etc

--- src/pages/learn.js
+++ src/pages/learn.js
@@ -1,13 +1,28 @@
 import * as React from 'react'
 import Layout from './layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import {
+    paragraphItems,
+    logoImage
+}
+from '../components/learn.module.css'
 
 const LearnPage = () => {
-
+   
     return(
         <Layout pageTitle={"Learning React"}>
+            <StaticImage className={logoImage}
+                alt="This is React Logo"
+                src="../images/react.png"
+            />
 
+            <p className={paragraphItems}> Event handler example:</p>
+
+            <form onSubmit={this.handleSubmit}>
+                <button type="submit"> Submit</button>
+            </form>
         </Layout>
-    )
+    );
 }
 
 export default LearnPage
